1c472082d4177e625e84ce38241cfb85bc7ace5b
Federico Arredondo <federicoarredondoc@gmail.com>
2021-12-16 23:29:23 -0300

    Add project files.

---
+++ SimplePartLoader/ModMain.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimplePartLoader
+{
+    public class ModMain : Mod
+    {
+        // Looking for docs? https://fedearre.github.io/my-garage-modding-docs/
+        public override string ID => "SimplePartLoader";
+        public override string Name => "SimplePartLoader";
+        public override string Author => "Federico Arredondo";
+        public override string Version => "dev";
+
+        public ModMain()
+        {
+
+        }
+
+        public override void OnLoad()
+        {
+
+        }
+
+        public override void Continue()
+        {
+
+        }
+
+        public override void OnSave()
+        {
+
+        }
+
+        public override void Update()
+        {
+
+        }
+    }
+}
