a82c9b8cfb45126360fb8b1ba33e0c21dd9365f2
Federico Arredondo <federicoarredondoc@gmail.com>
2021-12-18 02:32:15 -0300

    added new project in solution for testing

---
+++ SPL Tester/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SimplePartLoader;
+using UnityEngine;
+
+namespace SPL_Tester
+{
+    public class Class1 : Mod
+    {
+        // Looking for docs? https://fedearre.github.io/my-garage-modding-docs/
+        public override string ID => "Your mod's ID! (has to be unique)";
+        public override string Name => "Your mod's name";
+        public override string Author => "Your name";
+        public override string Version => "1.0";
+
+        public Class1()
+        {
+            AssetBundle bundle = AssetBundle.LoadFromMemory(Properties.Resources.spoiler_example);
+            Part p = SPL.LoadPart(bundle, "Spoiler");
+            p.SetupTransparent("TrunkDoor06", new Vector3(0f, 0f, 0f), Quaternion.Euler(0f, 0f, 0f));
+            bundle.Unload(false);
+        }
+    }
+}
