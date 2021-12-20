8f655059e9be17d3e64897de8d2beafdef578953
Federico Arredondo <federicoarredondoc@gmail.com>
2021-12-19 23:32:55 -0300

    tweak: removed debug message from ModMain

--- SimplePartLoader/ModMain.cs
+++ SimplePartLoader/ModMain.cs
@@ -15,27 +15,5 @@ namespace SimplePartLoader
         {
             PartManager.OnLoadCalled();
         }
-
-        public override void Update()
-        {
-            if (Input.GetKeyDown(KeyCode.M))
-            {
-                
-
-                // We have to load all our transparents into the game's cars prefabs. If we don't do this our part would not be attachable in any bought car.
-                GameObject carList = GameObject.Find("CarsParent");
-                GameObject[] cars = carList.GetComponent<CarList>().Cars;
-
-                foreach (GameObject car in cars)
-                {
-                    Transform[] childs = car.GetComponentsInChildren<Transform>();
-
-                    foreach (Transform child in childs)
-                    {
-                        Debug.LogError($"DEBUG: {child} in car {car.name}");
-                    }
-                }
-            }
-        }
     }
 }
