d605a315e89ba4bcf34664496a9fff2a72eff02f
Federico Arredondo <federicoarredondoc@gmail.com>
2021-12-21 02:14:35 -0300

    tweak: added 90 degree rotation for transparent editor and also modifier key was changed to Keypad Enter

--- SimplePartLoader/TransparentEdit.cs
+++ SimplePartLoader/TransparentEdit.cs
@@ -37,10 +37,10 @@ namespace SimplePartLoader
             // Show data from our transparent object.
             dataShown  = $"{gameObject.name} data";
             dataShown += $"\nActual mode: {(editingRotation ? "Rotation" : "Position")}";
-            dataShown += $"\nMultiplier status: {(Input.GetKey(KeyCode.LeftControl) ? "Pressed" : "Not pressed")}";
+            dataShown += $"\nMultiplier status: {(Input.GetKey(KeyCode.KeypadEnter) ? "Pressed" : "Not pressed")}";
             dataShown += $"\nLocal position: {gameObject.transform.localPosition.ToString("F3")}";
-            dataShown += $"\nLocal scale: {gameObject.transform.localScale.ToString("F3")}";
             dataShown += $"\nLocal rotation: {gameObject.transform.localEulerAngles.ToString("F3")}"; // F3 means 3 digit precision.
+            dataShown += $"\nLocal scale: {gameObject.transform.localScale.ToString("F3")}";
 
             if (Input.GetKeyDown(KeyCode.Keypad0)) // Multiplier
             {
@@ -50,44 +50,65 @@ namespace SimplePartLoader
             if (Input.GetKeyDown(KeyCode.Keypad1)) // X-
             {
                 if (editingRotation)
-                    gameObject.transform.localEulerAngles = actualRot - new Vector3( (Input.GetKey(KeyCode.LeftControl) ? 1f : 0.1f), 0f, 0f);
+                    gameObject.transform.localEulerAngles = actualRot - new Vector3( (Input.GetKey(KeyCode.KeypadEnter) ? 1f : 0.1f), 0f, 0f);
                 else
-                    gameObject.transform.localPosition = actualPos - new Vector3( (Input.GetKey(KeyCode.LeftControl) ? 0.1f : 0.01f), 0f, 0f);
+                    gameObject.transform.localPosition = actualPos - new Vector3( (Input.GetKey(KeyCode.KeypadEnter) ? 0.05f : 0.005f), 0f, 0f);
+            }
+            else if (Input.GetKeyDown(KeyCode.Keypad2)) // X 90
+            {
+                if (editingRotation)
+                    gameObject.transform.localEulerAngles = actualRot - new Vector3((Input.GetKey(KeyCode.KeypadEnter) ? 90f : -90f), 0f, 0f);
+                else
+                    gameObject.transform.localPosition = actualPos - new Vector3((Input.GetKey(KeyCode.KeypadEnter) ? 90f : -90f), 0f, 0f);
             }
             else if (Input.GetKeyDown(KeyCode.Keypad3)) // X+
             {
                 if (editingRotation)
-                    gameObject.transform.localEulerAngles = actualRot + new Vector3((Input.GetKey(KeyCode.LeftControl) ? 1f : 0.1f), 0f, 0f);
+                    gameObject.transform.localEulerAngles = actualRot + new Vector3((Input.GetKey(KeyCode.KeypadEnter) ? 1f : 0.1f), 0f, 0f);
                 else
-                    gameObject.transform.localPosition = actualPos + new Vector3((Input.GetKey(KeyCode.LeftControl) ? 0.1f : 0.01f), 0f, 0f);
+                    gameObject.transform.localPosition = actualPos + new Vector3((Input.GetKey(KeyCode.KeypadEnter) ? 0.05f : 0.005f), 0f, 0f);
             }
             else if (Input.GetKeyDown(KeyCode.Keypad4)) // Y-
             {
                 if (editingRotation)
-                    gameObject.transform.localEulerAngles = actualRot - new Vector3(0f, (Input.GetKey(KeyCode.LeftControl) ? 1f : 0.1f), 0f);
+                    gameObject.transform.localEulerAngles = actualRot - new Vector3(0f, (Input.GetKey(KeyCode.KeypadEnter) ? 1f : 0.1f), 0f);
                 else
-                    gameObject.transform.localPosition = actualPos - new Vector3(0f, (Input.GetKey(KeyCode.LeftControl) ? 0.1f : 0.01f), 0f);
+                    gameObject.transform.localPosition = actualPos - new Vector3(0f, (Input.GetKey(KeyCode.KeypadEnter) ? 0.05f : 0.005f), 0f);
+            }
+            else if (Input.GetKeyDown(KeyCode.Keypad5)) // Y 90
+            {
+                if (editingRotation)
+                    gameObject.transform.localEulerAngles = actualRot - new Vector3(0f, (Input.GetKey(KeyCode.KeypadEnter) ? 90f : -90f), 0f);
+                else
+                    gameObject.transform.localPosition = actualPos - new Vector3(0f, (Input.GetKey(KeyCode.KeypadEnter) ? 90f : -90f), 0f);
             }
             else if (Input.GetKeyDown(KeyCode.Keypad6)) // Y+
             {
                 if (editingRotation)
-                    gameObject.transform.localEulerAngles = actualRot + new Vector3(0f, (Input.GetKey(KeyCode.LeftControl) ? 1f : 0.1f), 0f);
+                    gameObject.transform.localEulerAngles = actualRot + new Vector3(0f, (Input.GetKey(KeyCode.KeypadEnter) ? 1f : 0.1f), 0f);
                 else
-                    gameObject.transform.localPosition = actualPos + new Vector3(0f, (Input.GetKey(KeyCode.LeftControl) ? 0.1f : 0.01f), 0f);
+                    gameObject.transform.localPosition = actualPos + new Vector3(0f, (Input.GetKey(KeyCode.KeypadEnter) ? 0.05f : 0.005f), 0f);
             }
             else if (Input.GetKeyDown(KeyCode.Keypad7)) // Z-
             {
                 if (editingRotation)
-                    gameObject.transform.localEulerAngles = actualRot - new Vector3(0f, 0f, (Input.GetKey(KeyCode.LeftControl) ? 1f : 0.1f));
+                    gameObject.transform.localEulerAngles = actualRot - new Vector3(0f, 0f, (Input.GetKey(KeyCode.KeypadEnter) ? 1f : 0.1f));
                 else
-                    gameObject.transform.localPosition = actualPos - new Vector3(0f, 0f, (Input.GetKey(KeyCode.LeftControl) ? 0.1f : 0.01f));
+                    gameObject.transform.localPosition = actualPos - new Vector3(0f, 0f, (Input.GetKey(KeyCode.KeypadEnter) ? 0.05f : 0.005f));
+            }
+            else if (Input.GetKeyDown(KeyCode.Keypad5)) // Z 90
+            {
+                if (editingRotation)
+                    gameObject.transform.localEulerAngles = actualRot - new Vector3(0f, 0f, (Input.GetKey(KeyCode.KeypadEnter) ? 90f : -90f));
+                else
+                    gameObject.transform.localPosition = actualPos - new Vector3(0f, 0f, (Input.GetKey(KeyCode.KeypadEnter) ? 90f : -90f));
             }
             else if (Input.GetKeyDown(KeyCode.Keypad9)) // Z+
             {
                 if (editingRotation)
-                    gameObject.transform.localEulerAngles = actualRot + new Vector3(0f, 0f, (Input.GetKey(KeyCode.LeftControl) ? 1f : 0.1f));
+                    gameObject.transform.localEulerAngles = actualRot + new Vector3(0f, 0f, (Input.GetKey(KeyCode.KeypadEnter) ? 1f : 0.1f));
                 else
-                    gameObject.transform.localPosition = actualPos + new Vector3(0f, 0f, (Input.GetKey(KeyCode.LeftControl) ? 0.1f : 0.01f));
+                    gameObject.transform.localPosition = actualPos + new Vector3(0f, 0f, (Input.GetKey(KeyCode.KeypadEnter) ? 0.05f : 0.005f));
             }
             else if (Input.GetKeyDown(KeyCode.Z))
             {
