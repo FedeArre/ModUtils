f7d86a35797fa92209729d80d0b8691da20dd780
Federico Arredondo <federicoarredondoc@gmail.com>
2022-04-04 23:51:41 -0300

    feat: added ModAPI class.

---
+++ SimplePartLoader/ModAPI.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace SimplePartLoader
+{
+    public class ModAPI
+    {
+        private static GameObject Player;
+        private static tools PlayerTools;
+        private static AudioManager AudioList;
+        private static AudioSource Source;
+
+        internal static void OnLoadCalled()
+        {
+            Player = GameObject.Find("Player");
+            PlayerTools = Player.GetComponent<tools>();
+
+            GameObject PlayerHand = GameObject.Find("hand");
+            AudioList = PlayerHand.GetComponent<AudioManager>();
+            Source = PlayerHand.GetComponent<AudioSource>();
+        }
+
+        public static GameObject GetPlayer()
+        {
+            if(Player)
+            {
+                return Player;
+            }
+            else
+            {
+                Debug.LogError("[SPL]: Tried to use GetPlayer but Player was null. Make sure that you are using it after OnLoad!");
+                return null;
+            }
+        }
+
+        public static tools GetPlayerTools()
+        {
+            if (PlayerTools)
+            {
+                return PlayerTools;
+            }
+            else
+            {
+                Debug.LogError("[SPL]: Tried to use GetPlayerTools but PlayerTools was null. Make sure that you are using it after OnLoad!");
+                return null;
+            }
+        }
+
+        public static AudioManager GetAudios()
+        {
+            if (AudioList)
+            {
+                return AudioList;
+            }
+            else
+            {
+                Debug.LogError("[SPL]: Tried to use GetAudios but the audio list was null. Make sure that you are using it after OnLoad!");
+                return null;
+            }
+        }
+
+        public static void PlaySound(AudioClip ac)
+        {
+            if(ac && Source)
+            {
+                Source.PlayOneShot(ac);
+            }
+            else
+            {
+                Debug.LogError("[SPL]: Tried to use PlaySound but AudioClip / source was null. Make sure that you are using it after OnLoad and have a valid AudioClip!");
+            }
+        }
+
+        public static void PlayCashSound()
+        {
+            if (Source)
+            {
+                Source.PlayOneShot(AudioList.Cash);
+            }
+        }
+    }
+}
