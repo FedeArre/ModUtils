fed3d56865d39c9fa2b2ecff36952db2e87423f2
Federico Arredondo <federicoarredondoc@gmail.com>
2022-05-27 23:43:33 -0300

    feat: WIP prefab generator from editor

---
+++ SimplePartLoader/Objects/EditorComponents/PrefabGenerator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace SimplePartLoader
+{
+    internal class PrefabGenerator : MonoBehaviour
+    {
+        public string PrefabName;
+        public string CopiesFrom;
+
+        public Mesh NewMesh;
+
+        public string PartName;
+
+        public int NewPrice = 0;
+        public bool EnablePartOnJunkyard = false;
+        public bool EnablePartOnCatalog = true;
+        public Texture2D CatalogImage;
+
+        public bool SavingFeatureEnabled;
+        public AttachmentTypes AttachmentType = AttachmentTypes.Default;
+
+        public enum AttachmentTypes
+        {
+            Default,
+            Prytool,
+            Hand
+        }
+    }
+
+}
